56236b99a8c0668ad8a4ce43ab3e95c0b41a949a
Yuvraj Kori <yuvrajkori29@gmail.com>
2024-04-30 15:51:25 +0530

    useState

--- prac/src/App.jsx
+++ prac/src/App.jsx
@@ -1,31 +1,35 @@
-import { Fragment } from "react"
-import './App.css';
-import { useRef } from "react";
 
+import { useState} from 'react'
+import {} from './App.css'
 
 function App() {
 
-// hook -useRef
+  ///use stattHook
+  const [count,setCount] = useState(0);
 
-const myRef = useRef();
-
-
-  const handleClick=()=>{
-    // console.log(myRef);
-
-
-//  document.body.style.backgroundColor = 'cyan';
-//  document.getElementById('btn').style.border = '1px solid black';
-//  document.getElementById('btn').style.color = 'black';
-
-
-myRef.current.style.backgroundColor ='salmon'  ;          //so we dont require id  and old javascript  methods.
+  const Increment = ()=>{
+       setCount(()=>count+1);
   }
+
+
+  const Decrement = ()=>{
+    if(count >0)
+    setCount(()=>count-1);
+  }
+  const Reset = ()=>{
+    setCount(()=>0);
+  }
+
   return (
-    <Fragment >
-      <div className ='border-1 border-white py-1 px-5 text-black hover:bg-cyan-50 hover:text-black ' id="btn"
-       onClick={handleClick} ref ={myRef}>Hooks</div>
-    </Fragment>
+    <div className='flex flex-col  h-screen justify-center items-center  '>
+    <h1 className='border-2 border-black-700 bg-yellow-400 py-2 px-3 m-6'>Counter : <span>{count}</span></h1>
+      <div className='flex justify-center items-center gap-6'>
+      <button className='border-2 border-black-700 py-1 px-4 bg-yellow-400'   onClick={Increment}>   Increment +</button>
+       <button  className='border-2 border-black-700 py-1 px-4  bg-yellow-400'   onClick={Decrement}>Decrement -</button>
+       <button  className='border-2 border-black-700 py-1 px-4  bg-yellow-400'   onClick={Reset}>Reset</button>
+      </div>
+       
+    </div>
   )
 }
 
