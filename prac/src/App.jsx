6383ec5e15bf1e7748367d07da8ca1aa151b006e
Yuvraj Kori <yuvrajkori29@gmail.com>
2024-05-01 18:16:49 +0530

    componentlifeCycle method

--- prac/src/App.jsx
+++ prac/src/App.jsx
@@ -1,33 +1,9 @@
-import {Fragment, useRef} from 'react'; 
 
-function App() { 
 
-// Creating a ref object using useRef hook 
-const focusPoint = useRef(); 
-const myRef= useRef(); 
-const onClickHandler = () => { 
-	focusPoint.current.value = 
-	"LEarning useRef hook"; 
-	focusPoint.current.focus(); 
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
 
-  console.log(myRef.current.value);
-}; 
-return ( 
-	<Fragment> 
-	<div> 
-		<button onClick={onClickHandler}> 
-		ACTION 
-		</button> 
-	</div> 
-	<label> 
-	Click on the action button to 
-	focus and populate the text. 
-	</label><br/> 
-	{/* <textarea ref={focusPoint} />  */}
-  <input type="text" ref={myRef} />
-  <button onClick={onClickHandler}></button>
-	</Fragment> 
-)
-} 
-
-export default App;
+export default App
