707874a94ae22e7114acff9c843d41848e9782f1
Yuvraj Kori <yuvrajkori29@gmail.com>
2024-05-02 17:05:38 +0530

    constructor

--- prac/src/App.jsx
+++ prac/src/App.jsx
@@ -1,9 +1,29 @@
+import  { Component } from 'react'
+import './App.css'
+export default class App extends Component {
+
+  //constructor\
+  constructor(){
+    super()
+    this.state = {
+      userName : "Ram",
+      userAge : 10,
+      gender : 'Male',
+      city : 'XYZ'
+    }
+  }
 
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+  render() {
+    return (
+      <div className='text-lg'>
+        <h2>User-Name is {this.state.userName} </h2>
+        <h2>Age : {this.state.userAge}</h2>
+        <h2>Gender: {this.state.gender}</h2>
+        <h2>City : {this.state.city}</h2>
+
+        <button className='bg-purple-400 text-white p-4 rounded-md text-lg' onClick={()=>{this.setState({age:24,city:'Delhi'})}}>Change city</button>
+      </div>
+    )
+  }
 }
-
-export default App
